4686c8ffef8e94d53b1f6af2aea540fd7afe3308
Constantin Rohde <rohdeconstantin@gmail.com>
2023-03-07 22:53:20 +0100

    added employment section and computer interests with own design

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
 
   let projectsButton = document.getElementById("projectslink");
+  let aboutButton = document.getElementById("aboutlink");
 
   projectsButton.addEventListener("click", () => {
     let projects = document.getElementById("projects");
     projects.scrollIntoView();
+  });
+
+  aboutButton.addEventListener("click", () => {
+    let about = document.getElementById("computers");
+    about.scrollIntoView();
   });
 
 });
